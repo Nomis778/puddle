895ff92f554b78edd3f6f6bf41d2c93de69eb5cd
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-05-28 21:59:21 +0200

    [ADD] getUserById

---
+++ puddle-server/src/main/java/com/github/nomis778/puddle/server/user/UserService.java
@@ -0,0 +1,24 @@
+package com.github.nomis778.puddle.server.user;
+
+import com.github.nomis778.puddle.server.user.model.PublicUser;
+import com.github.nomis778.puddle.server.user.model.User;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(long id) {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty())
+            throw new EntityNotFoundException("Error: User does not exist!");
+        return user.get();
+    }
+}
