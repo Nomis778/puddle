53792646c173603a2cd5a57488f12291bcc4d963
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-05-30 04:04:16 +0200

    [UPDATE] match view and selection split between controller and service correctly

--- puddle-client/src/main/java/com/github/nomis778/puddle/client/ChatViewController.java
+++ puddle-client/src/main/java/com/github/nomis778/puddle/client/ChatViewController.java
@@ -1,25 +1,71 @@
 package com.github.nomis778.puddle.client;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
 
 public class ChatViewController implements Initializable {
     private final MatchService matchService = new MatchService();
+    private ArrayList<ListView<Match>> allListViews = new ArrayList<>();
 
     @FXML
     VBox matchBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        matchService.refreshMatches(matchBox);
+        refreshMatchBox();
     }
 
     @FXML
     public void refreshMatchBox() {
-        matchService.refreshMatches(matchBox);
+        CompletableFuture
+                .supplyAsync(matchService::fetchMatchesByCompetition)
+                .thenAccept(result -> Platform.runLater(() -> populateBox(result)));
+    }
+
+    private void populateBox(Map<String, List<Match>> matchesByCompetition) {
+        clearBox();
+
+        matchesByCompetition.forEach((competition, matchList) -> {
+            ListView<Match> listView = new ListView<>();
+            listView.getItems().addAll(matchList);
+            listView.setCellFactory(lv -> new MatchListCell());
+
+            listView.getSelectionModel().selectedItemProperty()
+                    .addListener((obs, oldVal, newVal) -> {
+                        if (newVal != null) onMatchSelected(listView, newVal);
+                    });
+            allListViews.add(listView);
+
+            TitledPane pane = new TitledPane(competition, listView);
+            matchBox.getChildren().add(pane);
+        });
+    }
+
+    private void onMatchSelected(ListView<Match> source, Match match) {
+        matchService.selectedMatchIdProperty().setValue(match.id());
+
+        // Has to clear all other listview selections to
+        // allow for new selections on these listviews
+        for(var lv: allListViews) {
+            if(lv != source)
+                lv.getSelectionModel().clearSelection();
+        }
+    }
+
+    private void clearBox() {
+        allListViews.clear();
+        matchBox.getChildren().clear();
     }
 }
--- puddle-client/src/main/java/com/github/nomis778/puddle/client/MatchService.java
+++ puddle-client/src/main/java/com/github/nomis778/puddle/client/MatchService.java
@@ -1,6 +1,8 @@
 package com.github.nomis778.puddle.client;
 
 import com.github.nomis778.puddle.client.shared.HttpSession;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
@@ -14,66 +16,29 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MatchService {
-    private Map<String, List<Match>> matchesByCompetition;
-    private ArrayList<ListView<Match>> allListViews = new ArrayList<>();
-    private long selectedMatchId;
+    private final ObjectProperty<Long> selectedMatchId = new SimpleObjectProperty<>();
 
-    public void refreshMatches(VBox box) {
-        updateMatches();
-        clear(box);
-        populateBoxWithMatches(box);
-    }
-
-    public Match getSelectedMatch() {
-        RestClient client = HttpSession.getClient();
-        ResponseEntity<Match> matches = client.get()
-                .uri("/matches/" + selectedMatchId)
-                .retrieve()
-                .toEntity(Match.class);
-        return matches.getBody();
-    }
-
-    private void updateMatches() {
+    public Map<String, List<Match>> fetchMatchesByCompetition() {
         RestClient client = HttpSession.getClient();
         ResponseEntity<Match[]> matches = client.get()
                 .uri("/matches")
                 .retrieve()
                 .toEntity(Match[].class);
 
-        matchesByCompetition = Arrays.stream(matches.getBody())
+        return Arrays.stream(matches.getBody())
                 .collect(Collectors.groupingBy((match) -> match.competition().name()));
     }
 
-    private void clear(VBox box) {
-        allListViews.clear();
-        box.getChildren().clear();
+    public Match fetchSelectedMatch() {
+        RestClient client = HttpSession.getClient();
+        ResponseEntity<Match> matches = client.get()
+                .uri("/matches/" + selectedMatchId.get())
+                .retrieve()
+                .toEntity(Match.class);
+        return matches.getBody();
     }
 
-    private void populateBoxWithMatches(VBox box) {
-        matchesByCompetition.forEach((competition, matchList) -> {
-            ListView<Match> listView = new ListView<>();
-            listView.getItems().addAll(matchList);
-            listView.setCellFactory(lv -> new MatchListCell());
-
-            listView.getSelectionModel().selectedItemProperty()
-                    .addListener((obs, oldVal, newVal) -> {
-                        if (newVal != null) onMatchSelected(listView, newVal);
-                    });
-            allListViews.add(listView);
-
-            TitledPane pane = new TitledPane(competition, listView);
-            box.getChildren().add(pane);
-        });
-    }
-
-    private void onMatchSelected(ListView<Match> source, Match match) {
-        selectedMatchId = match.id();
-
-        // Has to clear all other listview selections to
-        // allow for new selections on these listviews
-        for(var lv: allListViews) {
-            if(lv != source)
-                lv.getSelectionModel().clearSelection();
-        }
+    public ObjectProperty<Long> selectedMatchIdProperty() {
+        return selectedMatchId;
     }
 }
